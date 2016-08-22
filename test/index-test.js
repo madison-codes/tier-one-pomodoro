9339728d9218cabad26da485a182665e3dd92d75
Casey Cross <hellocaseycross@gmail.com>
2016-08-22 17:51:03 -0600

    Initialize webpack and webdriver test bundles

---
+++ test/index-test.js
@@ -0,0 +1,9 @@
+'use strict';
+const assert = require('chai').assert;
+// require('./other-test-file') 
+
+describe('our test bundle', function () {
+  it('should work', function () {
+    assert(true);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './lib/index.js',
+    test: 'mocha!./test/index-test.js'
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      // { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
