7946bf3e3f1fef44a93dcf7b75afcfd8d0630368
Casey Cross <hellocaseycross@gmail.com>
2016-08-22 18:43:01 -0600

    First feature test passed.

--- test/specs/feature-test.js
+++ test/specs/feature-test.js
@@ -5,6 +5,6 @@ describe('welcome page', function(){
   it('should be able to grab the page title', function(){
   	browser.url('/');
   	var title = browser.getTitle();
-  	assert.equal(title, 'hello world');
+  	assert.equal(title, 'Pomodoro');
   });
 });
