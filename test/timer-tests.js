a88feb66abd9254b17c80b010c36e205e8c4b2a2
Casey Cross <hellocaseycross@gmail.com>
2016-08-25 16:01:18 -0600

    Write tests for all existing timer functions.

--- test/timer-tests.js
+++ test/timer-tests.js
@@ -2,7 +2,6 @@ const assert = require('chai').assert;
 const Timer = require('../lib/timer');
 
 describe('Timer', function() {
-
   var timer = new Timer();
 
   it('should be a function', function() {
@@ -22,19 +21,35 @@ describe('Timer', function() {
   });
 
   it('should check the time input and set a duration based on user input', function() {
-
     var input = 5;
     timer.checkTimeInput(input);
-
     assert.equal(timer.focusTimeDuration, 300000);
   });
 
-  // it('should calculate and end time if using the default start time', function() {
-  //   var input = '';
-  //   var output = timer.setEndTime(input);
-  //   assert.equal(output, 1500000);
-  //
-  // });
+  it('should add both the start time and duration to determine the appropriate end time', function() {
+    timer.focusTimeStart = 10;
+    timer.focusTimeDuration = 10;
+    timer.setEndTime();
+    assert.equal(timer.focusTimeEnd, 20);
+  });
 
+  it('should return time end minus time start to keep track of how much time remains', function() {
+    timer.focusTimeStart = 10;
+    timer.focusTimeEnd = 20;
+    timer.setTimeLeft();
+    assert.equal(timer.timeLeft, 10);
+  });
+
+  it('should reduce time left by one', function() {
+    timer.timeLeft = 5;
+    timer.incrementTimeLeft();
+    assert.equal(timer.timeLeft, 4);
+  });
+
+  it('should stop time if timeLeft equals zero', function() {
+    timer.timeLeft = 0;
+    var stopTime = timer.checkTimeStop();
+    assert.equal(stopTime, 'time has stopped');
+  });
 
 });
