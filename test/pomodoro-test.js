487609e95298449d291ab9c038245a3e48cecbb5
Madison Kerndt <madison.kerndt@gmail.com>
2016-08-28 17:50:36 -0600

    pomodoro object methods tests passing

--- test/pomodoro-test.js
+++ test/pomodoro-test.js
@@ -3,6 +3,14 @@ const Pomodoro = require('../lib/pomodoro');
 const Timer = require('../lib/timer');
 
 describe('Pomodoro', function() {
+  var array = Pomodoro.pomodoroArray;
+  beforeEach(function() {
+    array.push(new Timer());
+  });
+
+  afterEach(function() {
+    array.splice(0, array.length);
+  });
 
   context('instantiate timer within TimerRoom array', function(){
 
@@ -48,16 +56,81 @@ describe('Pomodoro', function() {
       assert.equal(array.length, 8);
     });
   });
-});
 
-describe('Add timer', function() {
-  var array1 = Pomodoro.pomodoroArray;
-  context('createDefaultWorkTimer', function(){
-    it('should create a new object', function(){
+  context('default work object', function(){
+    var array = Pomodoro.pomodoroArray;
+
+    it('should create a new default work object', function(){
+      assert.equal(array.length, 1);
+    });
+
+    it('should have a duration of 25 min', function(){
       Pomodoro.createDefaultWorkTimer();
-      assert.equal(array1.length,1);
+      assert.equal(array[1].duration, 1500000);
+    });
+
+    it('should have a session of work', function(){
+      Pomodoro.createDefaultWorkTimer();
+      assert.equal(array[1].session, 'work');
     });
   });
+
+  context('default rest object', function(){
+    var array = Pomodoro.pomodoroArray;
+
+    it('should create a new default work object', function(){
+      assert.equal(array.length, 1);
+    });
+
+    it('should have a duration of 5 min', function(){
+      Pomodoro.createDefaultRestTimer();
+      assert.equal(array[1].duration, 300000);
+    });
+
+    it('should have a session of rest', function(){
+      Pomodoro.createDefaultRestTimer();
+      assert.equal(array[1].session, 'rest');
+    });
+  });
+
+  context('custom work object', function(){
+    var array = Pomodoro.pomodoroArray;
+    var duration = 20;
+
+    it('should create a new default work object', function(){
+      assert.equal(array.length, 1);
+    });
+
+    it('should have a duration of 25 min', function(){
+      Pomodoro.createCustomWorkTimer(duration);
+      assert.equal(array[1].duration, 20);
+    });
+
+    it('should have a session of work', function(){
+      Pomodoro.createDefaultWorkTimer(duration);
+      assert.equal(array[1].session, 'work');
+    });
+  });
+
+  context('custom rest object', function(){
+    var array = Pomodoro.pomodoroArray;
+    var duration = 20;
+
+    it('should create a new default work object', function(){
+      assert.equal(array.length, 1);
+    });
+
+    it('should have a duration of 25 min', function(){
+      Pomodoro.createCustomWorkTimer(duration);
+      assert.equal(array[1].duration, 20);
+    });
+
+    it('should have a session of work', function(){
+      Pomodoro.createDefaultWorkTimer(duration);
+      assert.equal(array[1].session, 'work');
+    });
+  });
+
 });
 
 //
