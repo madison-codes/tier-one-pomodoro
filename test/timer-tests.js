c48941f067f1014e3e5bf621cc921edfea0e16b6
Madison Kerndt <madison.kerndt@gmail.com>
2016-08-27 19:13:08 -0600

    timer constructor functions complete

--- test/timer-tests.js
+++ test/timer-tests.js
@@ -4,7 +4,6 @@ const Timer = require('../lib/timer');
 
 
 describe('Timer', function() {
-  var timer = new Timer();
 
     beforeEach(function() {
       this.rightNow = Date.now();
@@ -16,7 +15,9 @@ describe('Timer', function() {
       Date.now = this.dateNow;
     });
 
-  context('should start and end', function(){
+  context('1', function(){
+    var timer = new Timer();
+
     it('should be a function', function() {
       assert.isFunction(Timer);
     });
@@ -39,67 +40,43 @@ describe('Timer', function() {
       assert.equal(end, expected);
     });
   });
-  context('should start and end', function(){
-    it('should return the start time if the start function has been called', function(){
-      timer.start();
-      assert.isTrue(timer.hasBeenStarted());
+  context('2', function(){
+    var timer = new Timer();
 
     it('should return false if the timer start function has not been started', function(){
-      assert.isFalse(timer.hasBeenStarted());
+      var outcome = timer.hasStarted();
+      assert.isFalse(outcome);
+    });
+
+    it('should return the start time if the start function has been called', function(){
+      timer.start();
+      var outcome = timer.hasStarted();
+      assert.isTrue(outcome);
+    });
+
+    it('should return the remaining time, difference between the current time and the set end time', function() {
+      var remaining = timer.remaining();
+      var expected = timer.end() - Date.now();
+      assert.equal(remaining, expected);
+    });
+
+    it('should return the elapsed time, difference between the current time and the start time', function() {
+      var remaining = timer.elapsed();
+      var expected = Date.now() - timer.start();
+      assert.equal(remaining, expected);
+    });
+  });
+  context('time expired', function() {
+    var timer = new Timer();
+
+    it('should return true if the time remaining is less than 0', function() {
+      timer.remaining = () => -1;
+      assert.isTrue(timer.expired());
+    });
+
+    it('should return false if the time remaining is greater than 0', function() {
+      timer.remaining = () => 1;
+      assert.isFalse(timer.expired());
     });
   });
 });
-
-
-
-  //   beforeEach(() => {
-  //     this.rightNow = Date.now();
-  //     this.dateNow = Date.now();
-  //     Date.now = () => this.rightNow;
-  //   });
-  //
-  //   afterEach(() => {
-  //     Date.now = this.dateNow;
-  //   });
-
-
-
-
-  //
-  // it('should be able to set a duration', function() {
-  //   var input = 1000;
-  //   timer.setDuration(input);
-  //   assert.equal(timer.focusTimeDuration, 60000000);
-  // });
-  //
-  // it('should check the time input and set a duration based on user input', function() {
-  //   var input = 5;
-  //   timer.checkTimeInput(input);
-  //   assert.equal(timer.focusTimeDuration, 300000);
-  // });
-  //
-  // it('should add both the start time and duration to determine the appropriate end time', function() {
-  //   timer.focusTimeStart = 10;
-  //   timer.focusTimeDuration = 10;
-  //   timer.setEndTime();
-  //   assert.equal(timer.focusTimeEnd, 20);
-  // });
-  //
-  // it('should return time end minus time start to keep track of how much time remains', function() {
-  //   timer.focusTimeStart = 10;
-  //   timer.focusTimeEnd = 20;
-  //   timer.setTimeLeft();
-  //   assert.equal(timer.timeLeft, 10);
-  // });
-  //
-  // it('should reduce time left by one', function() {
-  //   timer.timeLeft = 5;
-  //   timer.incrementTimeLeft();
-  //   assert.equal(timer.timeLeft, 4);
-  // });
-  //
-  // it('should stop time if timeLeft equals zero', function() {
-  //   timer.timeLeft = 0;
-  //   var stopTime = timer.checkTimeStop();
-  //   assert.equal(stopTime, 'time has stopped');
-  // });
