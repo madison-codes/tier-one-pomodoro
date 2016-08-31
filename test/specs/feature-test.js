9c1cc06cd562f94462699f2389569e15c1142d16
Casey Cross <hellocaseycross@gmail.com>
2016-08-31 13:33:51 -0600

    Test the tests

--- test/specs/feature-test.js
+++ test/specs/feature-test.js
@@ -3,40 +3,67 @@ const assert = require('assert');
 
 describe('Welcome page, button functionality', function(){
     browser.url('/');
+
   it('should be able to grab the page title', function(){
   	var title = browser.getTitle();
   	assert.equal(title, 'Pomodoro');
   });
 
-  it('should have a pragraph tag to display the remaining countdown time', function() {
-    var timer = browser.element('#display-count-down');
+  it('should have a paragraph tag to display the remaining countdown time', function() {
+    var timer = browser.element('.display-count-down');
     assert(timer.isExisting(''));
   });
 
-  it('timer button should not have class pause on game start', function() {
-    var buttonStatus = browser.isEnabled('#start-button');
-    assert.equal(buttonStatus, true);
-  });
+  // it('timer button should not have class pause on game start', function() {
+  //   var buttonStatus = browser.isEnabled('#start-button');
+  //   assert.equal(buttonStatus, true);
+  // });
 
-  it('pause button should be disabled on game start', function() {
-    var buttonStatus = browser.isEnabled('#pause-button');
-    assert.equal(buttonStatus, false);
-  });
+  // it('pause button should be disabled on game start', function() {
+  //   var buttonStatus = browser.isEnabled('#pause-button');
+  //   assert.equal(buttonStatus, false);
+  // });
 
   it('start button should be hidden on click of start button', function() {
-    browser.click('#start-button');
-    assert.equal('#startButton'.hide(), true);
+    browser.click('.start-button');
+    assert.equal(('.start-button').isDisplayed(), false);
+  });
+
+  it('pause button should be hidden on click of pause button', function() {
+    browser.click('.pause-button');
+    assert.isVisible('.pause-button');
+  });
+
+  it('resume button should be hidden on click of resume button', function() {
+    browser.click('.resume-button');
+    assert.property(('.resume-button').hide(), true);
   });
 
   it('pause button enabled on click of start button', function() {
-    browser.click('#start-button');
-    var buttonStatus = browser.isEnabled('#pause-button');
+    browser.click('.start-button');
+    var buttonStatus = browser.isEnabled('.pause-button');
     assert.equal(buttonStatus, true);
   });
-});
 
-// A default setting of 25 minutes of work followed by 5 minutes of break.
-// The ability to set custom lengths of time for both work and breaks.
-// If a custom length is set, it will stay at that new setting until it is set back to default.
-// A countdown alert that pings every 5 seconds for the final 20 seconds of both work and break, ending in an alert sound to signal the time has run out.
-// Visual indication of the time countdowns, including an indicator with more urgency/importance showing the final 20 second countdown.
+  it('should have an input for setting break time', function() {
+    var pomodoroTimeInput = browser.element('#focus-time-input');
+    assert(pomodoroTimeInput.isExisting(''));
+  });
+
+  it('should have an input for setting break time', function() {
+    var breakTimeInput = browser.element('#break-time-input');
+    assert(breakTimeInput.isExisting(''));
+  });
+
+  it('should register the value of the input for setting pomodoro focus time', function() {
+    var pomodoroTimeInput = browser.element('#focus-time-input');
+    pomodoroTimeInput.setValue(25);
+    assert.equal(pomodoroTimeInput.getValue(), 25);
+  });
+
+  it('should register the value of the input for setting pomodoro break time', function() {
+    var breakTimeInput = browser.element('#break-time-input');
+    breakTimeInput.setValue(5);
+    assert.equal(breakTimeInput.getValue(), 5);
+  });
+});
