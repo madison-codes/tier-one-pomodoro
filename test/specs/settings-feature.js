23d8ccd460d3db10f459461d799d21c12dfb3171
Casey Cross <hellocaseycross@gmail.com>
2016-12-26 11:33:19 -0500

    Clean up test file formatting.

--- test/specs/settings-feature.js
+++ test/specs/settings-feature.js
@@ -1,27 +1,27 @@
 'use strict';
 const assert = require('assert');
 
-describe('settings options', function() {
+describe('settings options', function () {
   browser.url('/');
 
-  it('should have an input for setting break time', function() {
+  it('should have an input for setting break time', function () {
     browser.click('#settings-link');
     var pomodoroTimeInput = browser.element('#focus-time-input');
     assert(pomodoroTimeInput.isExisting(''));
   });
 
-  it('should have an input for setting break time', function() {
+  it('should have an input for setting break time', function () {
     var breakTimeInput = browser.element('#break-time-input');
     assert(breakTimeInput.isExisting(''));
   });
 
-  it('should register the value of the input for setting pomodoro focus time', function() {
+  it('should register the value of the input for setting pomodoro focus time', function () {
     var pomodoroTimeInput = browser.element('#focus-time-input');
     pomodoroTimeInput.setValue(25);
     assert.equal(pomodoroTimeInput.getValue(), 25);
   });
 
-  it('should register the value of the input for setting pomodoro break time', function() {
+  it('should register the value of the input for setting pomodoro break time', function () {
     var breakTimeInput = browser.element('#break-time-input');
     breakTimeInput.setValue(5);
     assert.equal(breakTimeInput.getValue(), 5);
